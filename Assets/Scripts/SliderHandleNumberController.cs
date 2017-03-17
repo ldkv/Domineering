1ff7a7c026855b661d05a06341949c74168da456
charline_rochelle <charline.rochelle@gmail.com>
2017-03-17 12:03:00 +0100

    ui - COLORS ARE COMING

---
+++ Assets/Scripts/SliderHandleNumberController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine . UI;
+
+[RequireComponent ( typeof ( Slider ) )]
+public class SliderHandleNumberController : MonoBehaviour
+{
+    [SerializeField]
+    private Slider _slide;
+    [SerializeField]
+    private Text _text;
+
+    // Use this for initialization
+    void Start ( )
+    {
+        OnChangeValue ( );
+    }
+
+    public void OnChangeValue ( )
+    {
+        this . _text . text = this . _slide . value . ToString ( );
+    }
+}
